c3d137d194945a9468efb6e7a5f26b4985021249
Andrii Porokhnavets <andrij843@gmail.com>
2019-05-05 16:29:53 +0300

    Add validation to Score model

--- backend/scores/models.py
+++ backend/scores/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Score(models.Model):
     game = models.ForeignKey('games.Game', on_delete=models.CASCADE)
     who = models.ForeignKey('players.Player', on_delete=models.CASCADE, related_name='who')
     whom = models.ForeignKey('players.Player', on_delete=models.CASCADE, related_name='whom')
-    value = models.DecimalField(max_digits=3, decimal_places=1)
+    value = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(0.1),
+            MaxValueValidator(10),
+        ]
+    )
--- backend/scores/serializers.py
+++ backend/scores/serializers.py
@@ -1,8 +1,21 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from .models import Score
 
 
 class ScoreSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if data['who'] == data['whom']:
+            raise serializers.ValidationError('Player can not rate himself')
+
+        return data
+
     class Meta:
         model = Score
         fields = '__all__'
+
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Score.objects.all(),
+                fields=('who', 'whom')
+            )
+        ]
